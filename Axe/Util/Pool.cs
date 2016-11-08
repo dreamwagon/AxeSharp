b0dc9383e0b6a3cde4527c53af1f4b6e6a3bff66
dreamwagon <dreamwagongames@gmail.com>
2016-11-08 11:06:20 -0500

    Add files via upload

---
+++ Axe/Util/Pool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.dreamwagon.axe
+{
+    public delegate T Factory<T>();
+
+    public class Pool<T>
+    {
+
+        public int size;
+        public T[] stack;
+        public Factory<T> factory;
+
+        public Pool(T[] stack, Factory<T> factory)
+        {
+            this.stack = stack;
+            this.factory = factory;
+        }
+        public T alloc()
+        {
+            return (size == 0 ? factory() : stack[--size]);
+        }
+        public void free(T item)
+        {
+            if (size < stack.Length)
+            {
+                stack[size++] = item;
+            }
+        }
+    }
+}
